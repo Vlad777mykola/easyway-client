1d43e8166891c836a8b79a93b6b9aecfef96d68e
Tolik9924 <sakurasoft9924@gmail.com>
2024-12-18 01:56:01 +0200

    refactor(rendering navbar): showing left and right sides of navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,28 +1,14 @@
 import { ReactNode } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '../../ui-components/Button/Button.tsx';
 import { classes } from '../../common_utils/classes/classes.tsx';
 import styles from './Navbar.module.css';
 
-interface NavbarItem {
-	href: string;
-	children: ReactNode | string;
-	isModal: boolean;
-}
-
 export type Props = {
 	isSticky?: boolean;
-	rightItems?: NavbarItem[];
-	leftItems?: NavbarItem[];
+	RightSide: ReactNode;
+	LeftSide: ReactNode;
 };
 
-const Navbar = ({ isSticky = false, rightItems = [], leftItems = [] }: Props) => {
-	const navigate = useNavigate();
-
-	const handleClick = (link: string) => {
-		navigate(link);
-	};
-
+const Navbar = ({ isSticky = false, RightSide = <>Hi</>, LeftSide = <>Hi</> }: Props) => {
 	return (
 		<nav
 			className={classes(styles.navContainer, {
@@ -31,38 +17,8 @@ const Navbar = ({ isSticky = false, rightItems = [], leftItems = [] }: Props) =>
 			})}
 		>
 			<div className={styles.navResponsive}>
-				<div className={styles.rightSide}>
-					{leftItems.length > 0 && (
-						<div className={styles.nav}>
-							{leftItems.map((item) => (
-								<li key={item.href} className={styles.navItem}>
-									<Button
-										type="link"
-										onClick={() => {
-											handleClick(item.href);
-										}}
-									>
-										{item.children}
-									</Button>
-								</li>
-							))}
-						</div>
-					)}
-				</div>
-				<ul className={styles.nav}>
-					{rightItems.map((rightItem) => (
-						<li key={rightItem.href} className={styles.navItem}>
-							<Button
-								type="link"
-								onClick={() => {
-									handleClick(rightItem.href);
-								}}
-							>
-								{rightItem.children}
-							</Button>
-						</li>
-					))}
-				</ul>
+				{LeftSide}
+				{RightSide}
 			</div>
 		</nav>
 	);
