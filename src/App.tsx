2b236bc8e8f25e98666e806bbbacdf18fc907327
Tolik9924 <sakurasoft9924@gmail.com>
2024-12-18 17:20:53 +0200

    refactor(App.tsx): make standart view and remove icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import { routers } from './router/Router';
-import { ShrinkOutlined } from '@ant-design/icons';
-import Icon from './ui-components/Icon/Icon';
 
 export const App = () => {
 	return (
 		<>
-			{/* <RouterProvider router={routers} /> */}
-			<Icon size="xl" variant="primary" IconSVG={<ShrinkOutlined />} />
+			<RouterProvider router={routers} />
 		</>
 	);
 };
