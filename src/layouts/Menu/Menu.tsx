e577bbf6ad5a0ddc85892058a49b8b7db94ece96
Tolik9924 <sakurasoft9924@gmail.com>
2024-12-21 16:11:35 +0200

    feat(layouts): add side menu

---
+++ src/layouts/Menu/Menu.tsx
@@ -0,0 +1,72 @@
+import { useState, ReactNode } from 'react';
+
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
+
+import styles from './menu.module.css';
+import { Button } from '@/ui-components/Button';
+import { classes } from '@/utils/classes';
+
+const Side = {
+	left: 'left',
+	right: 'right',
+};
+
+type SideType = keyof typeof Side;
+
+type Props = {
+	Items: ReactNode;
+	side: SideType;
+};
+
+export const Menu = ({ Items, side }: Props) => {
+	const [open, setOpen] = useState<boolean>(false);
+	const [notShow, setNotShow] = useState<boolean>(true);
+
+	const onClick = () => {
+		setOpen(!open);
+		setNotShow(false);
+	};
+
+	const handleCloseMenu = () => {
+		setOpen(false);
+		setNotShow(false);
+	};
+
+	return (
+		<>
+			<div
+				className={classes(styles.overlay, {
+					[styles.openOverlay]: open,
+					[styles.closeOverlay]: !open,
+				})}
+				onClick={handleCloseMenu}
+			></div>
+			{!notShow && (
+				<div
+					className={classes(styles.wrapper, {
+						[styles.openMenu]: open,
+						[styles.notShowMenu]: notShow,
+						[styles.closeMenu]: !open && !notShow,
+						[styles[Side[side]]]: !!side,
+					})}
+				>
+					<span
+						className={classes(styles.menuButton, {
+							[styles.showMenuItem]: open,
+							[styles.closeMenuItem]: !open && !notShow,
+							[styles.menuButtonRight]: side === 'right',
+							[styles.menuButtonLeft]: side === 'left',
+						})}
+						onClick={handleCloseMenu}
+					>
+						<CloseOutlined />
+					</span>
+					{Items}
+				</div>
+			)}
+			<Button size="middle" variant="filled" onClick={onClick}>
+				<MenuOutlined />
+			</Button>
+		</>
+	);
+};
--- src/layouts/main/Layouts.tsx
+++ src/layouts/main/Layouts.tsx
@@ -1,11 +1,28 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../navbar';
+import { Menu } from '../Menu';
 import styles from './layouts.module.css';
+
+const items = [<div>Menu 1</div>, <div>Menu 2</div>, <div>Menu 3</div>, <div>Menu 4</div>];
+
+const SideMenu = () => {
+	return (
+		<div>
+			<div className={styles.menuItem}>Menu 1</div>
+			<div className={styles.menuItem}>Menu 2</div>
+			<div className={styles.menuItem}>Menu 3</div>
+			<div className={styles.menuItem}>Menu 4</div>
+		</div>
+	);
+};
 
 const Layouts = () => (
 	<div className={styles.page}>
 		<header className={styles.header}>
-			<Navbar RightSide={<div>Menu</div>} LeftSide={<div>Menu</div>} />
+			<Navbar
+				RightSide={<Menu side="right" Items={<SideMenu />} />}
+				LeftSide={<Menu side="left" Items={<SideMenu />} />}
+			/>
 		</header>
 		<main className={styles.main}>
 			<Outlet />
