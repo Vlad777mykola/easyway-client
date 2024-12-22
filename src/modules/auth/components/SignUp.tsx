9f5bb49bcd07f0761d50b125c215207354789100
Tolik9924 <sakurasoft9924@gmail.com>
2024-12-22 16:37:10 +0200

    style(sign up): style sign up

--- src/modules/auth/components/SignUp.tsx
+++ src/modules/auth/components/SignUp.tsx
@@ -1,30 +1,113 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useCreateUser } from '../hooks/useCreateUser';
 import { useLogin } from '../hooks/useLogin';
-import { Auth } from './Auth';
+import { useGetUser } from '../hooks/useGetUser';
+import { Input } from '@/ui-components/Input';
+import { Button } from '@/ui-components/Button';
+import style from './signUp.module.css';
+
+interface Data {
+	email: string;
+	password: string;
+}
 
 export const SignUp = () => {
 	const [createUser] = useCreateUser();
 	const { login } = useLogin();
+	const navigate = useNavigate();
+	const { data } = useGetUser();
+	const [auth, setAuth] = useState({
+		email: '',
+		username: '',
+		password: '',
+		repeatPassword: '',
+	});
+
+	const onChange = (value: string, key: 'email' | 'username' | 'password' | 'repeatPassword') => {
+		setAuth((prev) => ({
+			...prev,
+			[key]: value,
+		}));
+	};
+
+	const onSubmit = async ({ email, password }: Data): Promise<void> => {
+		await createUser({
+			variables: {
+				createUserInput: {
+					email,
+					password,
+				},
+			},
+		});
+		await login({ email, password });
+	};
+
+	useEffect(() => {
+		if (data) {
+			navigate('/');
+		}
+	}, [data, navigate]);
 
 	return (
 		<>
-			<Auth
-				submitLabel={'SignUp'}
-				onSubmit={async ({ email, password }): Promise<void> => {
-					await createUser({
-						variables: {
-							createUserInput: {
-								email,
-								password,
-							},
-						},
-					});
-					await login({ email, password });
-				}}
-			>
-				<Link to={'/login'}>Login</Link>
-			</Auth>
+			<div className={style.authContainer}>
+				<div className={style.signUpForm}>
+					<h3 className={style.signUpHeader}>Sign Up</h3>
+					<div className={style.formItem}>
+						<p className={style.nameOfItem}>Username: </p>
+						<Input
+							placeholder="username"
+							value={auth.email}
+							size="large"
+							onChange={(e) => onChange(e.target.value, 'username')}
+						/>
+					</div>
+					<div className={style.formItem}>
+						<p className={style.nameOfItem}>Email: </p>
+						<Input
+							placeholder="email"
+							value={auth.email}
+							size="large"
+							onChange={(e) => onChange(e.target.value, 'email')}
+						/>
+					</div>
+					<div className={style.formItem}>
+						<p className={style.nameOfItem}>Password: </p>
+						<Input
+							placeholder="password"
+							value={auth.password}
+							size="large"
+							onChange={(e) => onChange(e.target.value, 'password')}
+						/>
+					</div>
+					<div className={style.formItem}>
+						<p className={style.nameOfItem}>Repeat Password: </p>
+						<Input
+							placeholder="repeat password"
+							value={auth.password}
+							size="large"
+							onChange={(e) => onChange(e.target.value, 'repeatPassword')}
+						/>
+					</div>
+					<div className={style.sendSignUp}>
+						<Button
+							type="primary"
+							onClick={() => onSubmit({ email: auth.email, password: auth.password })}
+							size="large"
+							block
+						>
+							Sign Up
+						</Button>
+					</div>
+				</div>
+				<div>
+					<Link className={style.login} to={'/login'}>
+						Login
+					</Link>
+				</div>
+			</div>
 		</>
 	);
 };
