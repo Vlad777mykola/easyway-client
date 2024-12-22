fe6dfe6df9cbf50b731a74cce8516891c9e49d7e
Tolik9924 <sakurasoft9924@gmail.com>
2024-12-22 15:54:52 +0200

    refactor(login): remove Auth component move Auth code to Login component

--- src/modules/auth/components/SignUp.tsx
+++ src/modules/auth/components/SignUp.tsx
@@ -1,30 +1,7 @@
-import { Link } from 'react-router-dom';
-import { useCreateUser } from '../hooks/useCreateUser';
-import { useLogin } from '../hooks/useLogin';
-import { Auth } from './Auth';
-
 export const SignUp = () => {
-	const [createUser] = useCreateUser();
-	const { login } = useLogin();
-
 	return (
 		<>
-			<Auth
-				submitLabel={'SignUp'}
-				onSubmit={async ({ email, password }): Promise<void> => {
-					await createUser({
-						variables: {
-							createUserInput: {
-								email,
-								password,
-							},
-						},
-					});
-					await login({ email, password });
-				}}
-			>
-				<Link to={'/login'}>Login</Link>
-			</Auth>
+			<div>Sign Up</div>
 		</>
 	);
 };
