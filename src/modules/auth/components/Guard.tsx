058fa3e78745017510f9ac2b242ab095334dbbd9
Tolik9924 <sakurasoft9924@gmail.com>
2024-12-24 21:52:43 +0200

    feature(creating test): create module of creating tests

--- src/modules/auth/components/Guard.tsx
+++ src/modules/auth/components/Guard.tsx
@@ -6,7 +6,7 @@ interface IGuardProps {
 	children: ReactNode;
 }
 
-const EXECUTED_ROUTES = ['/login', '/signup', '/'];
+const EXECUTED_ROUTES = ['/test', '/login', '/signup', '/'];
 
 export const Guard = ({ children }: IGuardProps) => {
 	const { data: user } = useGetUser();
