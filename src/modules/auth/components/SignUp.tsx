d4f0433a681a6d4a6d618fc8e53e8fe9b08a4b54
Tolik9924 <sakurasoft9924@gmail.com>
2025-01-03 22:54:45 +0200

    chore(wrapper): make wrapper for components

--- src/modules/auth/components/SignUp.tsx
+++ src/modules/auth/components/SignUp.tsx
@@ -7,6 +7,7 @@ import { useGetUser } from '../hooks/useGetUser';
 import { Input } from '@/ui-components/Input';
 import { Button } from '@/ui-components/Button';
 import style from './signUp.module.css';
+import { Wrapper } from './Wrapper';
 
 interface Data {
 	email: string;
@@ -62,84 +63,86 @@ export const SignUp = () => {
 
 	return (
 		<>
-			<div className={style.authContainer}>
-				<div className={style.signUpForm}>
-					<h3 className={style.signUpHeader}>Sign Up</h3>
-					<div className={style.switchRole}>
-						<div className={style.role}>
+			<Wrapper>
+				<div className={style.authContainer}>
+					<div className={style.signUpForm}>
+						<h3 className={style.signUpHeader}>Sign Up</h3>
+						<div className={style.switchRole}>
+							<div className={style.role}>
+								<Button
+									className={style.button}
+									onClick={() => changeRole('teacher')}
+									type="text"
+									disabled={auth.role === 'teacher'}
+								>
+									Teacher
+								</Button>
+							</div>
+							<div className={style.role}>
+								<Button
+									className={style.button}
+									onClick={() => changeRole('student')}
+									type="text"
+									disabled={auth.role === 'student'}
+								>
+									Student
+								</Button>
+							</div>
+						</div>
+						<div className={style.formItem}>
+							<p className={style.nameOfItem}>Username: </p>
+							<Input
+								placeholder="username"
+								value={auth.email}
+								size="large"
+								onChange={(e) => onChange(e.target.value, 'username')}
+							/>
+						</div>
+						<div className={style.formItem}>
+							<p className={style.nameOfItem}>Email: </p>
+							<Input
+								placeholder="email"
+								value={auth.email}
+								size="large"
+								onChange={(e) => onChange(e.target.value, 'email')}
+							/>
+						</div>
+						<div className={style.formItem}>
+							<p className={style.nameOfItem}>Password: </p>
+							<Input
+								placeholder="password"
+								value={auth.password}
+								size="large"
+								onChange={(e) => onChange(e.target.value, 'password')}
+							/>
+						</div>
+						<div className={style.formItem}>
+							<p className={style.nameOfItem}>Repeat Password: </p>
+							<Input
+								placeholder="repeat password"
+								value={auth.password}
+								size="large"
+								onChange={(e) => onChange(e.target.value, 'repeatPassword')}
+							/>
+						</div>
+						<div className={style.sendSignUp}>
 							<Button
-								className={style.button}
-								onClick={() => changeRole('teacher')}
-								type="text"
-								disabled={auth.role === 'teacher'}
+								type="primary"
+								onClick={() => onSubmit({ email: auth.email, password: auth.password })}
+								size="large"
+								block
 							>
-								Teacher
+								Sign Up
 							</Button>
 						</div>
-						<div className={style.role}>
-							<Button
-								className={style.button}
-								onClick={() => changeRole('student')}
-								type="text"
-								disabled={auth.role === 'student'}
-							>
-								Student
-							</Button>
-						</div>
 					</div>
-					<div className={style.formItem}>
-						<p className={style.nameOfItem}>Username: </p>
-						<Input
-							placeholder="username"
-							value={auth.email}
-							size="large"
-							onChange={(e) => onChange(e.target.value, 'username')}
-						/>
-					</div>
-					<div className={style.formItem}>
-						<p className={style.nameOfItem}>Email: </p>
-						<Input
-							placeholder="email"
-							value={auth.email}
-							size="large"
-							onChange={(e) => onChange(e.target.value, 'email')}
-						/>
-					</div>
-					<div className={style.formItem}>
-						<p className={style.nameOfItem}>Password: </p>
-						<Input
-							placeholder="password"
-							value={auth.password}
-							size="large"
-							onChange={(e) => onChange(e.target.value, 'password')}
-						/>
-					</div>
-					<div className={style.formItem}>
-						<p className={style.nameOfItem}>Repeat Password: </p>
-						<Input
-							placeholder="repeat password"
-							value={auth.password}
-							size="large"
-							onChange={(e) => onChange(e.target.value, 'repeatPassword')}
-						/>
-					</div>
-					<div className={style.sendSignUp}>
-						<Button
-							type="primary"
-							onClick={() => onSubmit({ email: auth.email, password: auth.password })}
-							size="large"
-							block
-						>
-							Sign Up
-						</Button>
+					<div className={style.loginContainer}>
+						<Link className={style.login} to={'/login'}>
+							Login
+						</Link>
 					</div>
 				</div>
-				<div className={style.loginContainer}>
-					<Link className={style.login} to={'/login'}>
-						Login
-					</Link>
-				</div>
-			</div>
+			</Wrapper>
 		</>
 	);
 };
