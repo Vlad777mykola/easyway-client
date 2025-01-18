51e036260e8c64382c55a7396460625bab5af3fa
Tolik9924 <sakurasoft9924@gmail.com>
2025-01-19 00:27:34 +0200

    refactor(random number function): change position of condition correction minNumber and maxNumber

--- src/shared/utils/getRandomInteger/getRandomInteger.ts
+++ src/shared/utils/getRandomInteger/getRandomInteger.ts
@@ -1,8 +1,8 @@
 export const getRandomInteger = (current: number, maxNumber: number, minNumber = 0): number => {
-	const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 	if (minNumber === maxNumber || minNumber > maxNumber) {
 		return minNumber;
 	}
+	const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 	if (randomNumber === current) {
 		return getRandomInteger(current, maxNumber, minNumber);
 	}
