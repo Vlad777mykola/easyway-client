1949dbe43fbb11c1f280b117149772d1134c7378
Tolik9924 <sakurasoft9924@gmail.com>
2025-01-18 19:35:15 +0200

    feat(locale storage): remake structure

--- src/common-utils/localStorage.ts
+++ src/common-utils/localStorage.ts
@@ -1,34 +1,28 @@
-export const setItem = (key: string, value: string) => {
-	try {
-		const serializedValue = JSON.stringify(value);
-		localStorage.setItem(key, serializedValue);
-	} catch (error) {
-		console.error('Error saving to localStorage', error);
-	}
-};
+export const localstorage = {
+	setItem: (key: string, value: unknown) => {
+		try {
+			const serializedValue = JSON.stringify(value);
+			localStorage.setItem(key, serializedValue);
+		} catch (error) {
+			console.error('Error saving to localStorage', error);
+		}
+	},
 
-export const getItem = (key: string) => {
-	try {
-		const serializedValue = localStorage.getItem(key);
-		return serializedValue ? JSON.parse(serializedValue) : null;
-	} catch (error) {
-		console.error('Error reading from localStorage', error);
-		return null;
-	}
-};
+	getItem: (key: string) => {
+		try {
+			const serializedValue = localStorage.getItem(key);
+			return serializedValue ? JSON.parse(serializedValue) : null;
+		} catch (error) {
+			console.error('Error reading from localStorage', error);
+			return null;
+		}
+	},
 
-export const removeItem = (key: string) => {
-	try {
-		localStorage.removeItem(key);
-	} catch (error) {
-		console.error('Error removing from localStorage', error);
-	}
-};
-
-export const clear = () => {
-	try {
-		localStorage.clear();
-	} catch (error) {
-		console.error('Error clearing localStorage', error);
-	}
+	removeItem: (key: string) => {
+		try {
+			localStorage.removeItem(key);
+		} catch (error) {
+			console.error('Error removing from localStorage', error);
+		}
+	},
 };
