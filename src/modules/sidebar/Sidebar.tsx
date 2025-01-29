599f1ab61591037f258bf63870fe259225a91663
Tolik9924 <sakurasoft9924@gmail.com>
2025-01-30 01:23:42 +0200

    chore(side bar): add react hook form for side bar

--- src/modules/sidebar/Sidebar.tsx
+++ src/modules/sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { Select } from '@/ui-components/Select';
 import { Button } from '@/ui-components/Button';
 import { Icon } from '@/ui-components/Icon';
 import { Wrapper } from '@/ui-components/Wrapper';
-import styles from './sidebar.module.css';
 import { Input } from '@/ui-components/Input';
 import { Checkbox } from '@/ui-components/Checkbox';
+import styles from './sidebar.module.css';
 
 export type SelectValue = {
 	[key: string]: string | boolean | string[];
@@ -25,14 +26,14 @@ type SelectData = {
 export type Props = {
 	title: string;
 	selectData: SelectData[];
-	onSearch: (data: SelectValue) => void;
-	onChange: (data: SelectValue) => void;
+	onSearch?: (data: SelectValue) => void;
+	onChange?: (data: SelectValue) => void;
 };
 
 const returnComponent = (
 	data: SelectData[],
-	selectValue: SelectValue,
-	handleChange: (key: string, value: string[] | string | boolean) => void,
+	setValue: (keyValue: string, value: string | boolean) => void,
+	watch: (keyValue: string) => string | boolean | string[],
 ) => {
 	const renderComponent = data.map((item) => {
 		if (item.componentType === 'select') {
@@ -41,9 +42,9 @@ const returnComponent = (
 					<Select
 						className={styles.select}
 						placeholder={item.label}
-						value={selectValue[item.keyValue]}
-						onChange={(value) => handleChange(item.keyValue as string, value as string[])}
 						options={[...item.selectData!.map((option) => ({ value: option, label: option }))]}
+						value={watch(item.keyValue)}
+						onChange={(value) => setValue(item.keyValue as string, value)}
 					/>
 				</div>
 			);
@@ -56,9 +57,9 @@ const returnComponent = (
 						className={styles.select}
 						mode={item.componentType}
 						placeholder={item.label}
-						value={selectValue[item.keyValue]}
-						onChange={(value) => handleChange(item.keyValue as string, value as string[])}
 						options={[...item.selectData!.map((option) => ({ value: option, label: option }))]}
+						value={watch(item.keyValue)}
+						onChange={(value) => setValue(item.keyValue as string, value)}
 					/>
 				</div>
 			);
@@ -69,11 +70,9 @@ const returnComponent = (
 				<div key={item.keyValue} className={styles.selectContainer}>
 					<Input
 						name="value"
-						value={selectValue[item.keyValue] as string}
 						placeholder={item.placeholder}
-						onChange={(event) =>
-							handleChange(item.keyValue as string, event.target.value as string)
-						}
+						value={watch(item.keyValue) as string}
+						onChange={(event) => setValue(item.keyValue as string, event.target.value as string)}
 					/>
 				</div>
 			);
@@ -83,10 +82,8 @@ const returnComponent = (
 			return (
 				<div key={item.keyValue} className={styles.selectContainer}>
 					<Checkbox
-						checked={selectValue[item.keyValue] as boolean}
-						onChange={(event) =>
-							handleChange(item.keyValue as string, event.target.checked as boolean)
-						}
+						onChange={(e) => setValue(item.keyValue as string, e.target.checked as boolean)}
+						checked={watch(item.keyValue) as boolean}
 					>
 						{item.label}
 					</Checkbox>
@@ -99,49 +96,40 @@ const returnComponent = (
 };
 
 export const Sidebar = ({ title, selectData, onSearch, onChange }: Props) => {
-	const [selectValue, setSelectValue] = useState<SelectValue>({});
+	const { handleSubmit, setValue, watch, reset } = useForm<SelectValue>();
+
+	const watchFields = watch();
 
 	useEffect(() => {
-		let defaultValues = {};
-		selectData?.forEach((item) => {
-			defaultValues = { ...defaultValues, [item.keyValue]: item.defaultValue };
-		});
-		setSelectValue({ ...defaultValues });
-	}, []);
-
-	const handleChange = (key: string, value: string[] | string | boolean) => {
-		console.log('KEY: ', key);
-		console.log('VALUE: ', value);
-		setSelectValue((prev) => ({ ...prev, [key]: value }));
 		if (onChange) {
-			onChange({ ...selectValue, [key]: value });
+			handleSubmit((data) => {
+				onChange(data);
+			})();
 		}
-	};
-
-	const clear = () => {
-		setSelectValue({});
-	};
+	}, [watchFields]);
 
 	return (
 		<Wrapper>
-			<div className={styles.filterContainer}>
-				<h1 className={styles.title}>{title}</h1>
-				{returnComponent(selectData, selectValue, handleChange)}
-			</div>
-			{onSearch && (
+			<form className={styles.formContainer} onSubmit={onSearch && handleSubmit(onSearch)}>
+				<div className={styles.filterContainer}>
+					<h1 className={styles.title}>{title}</h1>
+					{returnComponent(selectData, setValue, watch)}
+				</div>
 				<div className={styles.buttonsContainer}>
-					<div className={styles.buttonContainer}>
-						<Button block onClick={() => onSearch(selectValue)}>
-							<Icon icon="search" /> Search
-						</Button>
-					</div>
-					<div className={styles.buttonContainer}>
-						<Button block onClick={clear}>
+					{onSearch && (
+						<div className={styles.buttonContainer}>
+							<Button block htmlType="submit">
+								<Icon icon="search" /> Search
+							</Button>
+						</div>
+					)}
+					<div className={onSearch ? styles.buttonContainer : styles.fullwidthContainer}>
+						<Button block onClick={() => reset()}>
 							<Icon icon="clear" /> Clear
 						</Button>
 					</div>
 				</div>
-			)}
+			</form>
 		</Wrapper>
 	);
 };
