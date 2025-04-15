f7e3deb0626b6a74e9b4805e3a4448eb06ad81e7
Tolik9924 <sakurasoft9924@gmail.com>
2025-04-15 23:38:02 +0300

    chore(progress page): make progress page

--- src/modules/progress/Progress.tsx
+++ src/modules/progress/Progress.tsx
@@ -5,10 +5,9 @@ import { ProgressStoreState } from '@/store/progress';
 import { AddInfo } from './components/add-info/AddInfo';
 import { CommonInfo } from './components/total-info/CommonInfo';
 import { fetchProgressData } from './services/fetchProgressData';
-import { calculateProgress } from './services/calculateProgress';
+import { CollectionsType } from '@/shared/constants';
 
 import styles from './progress.module.css';
-import { StandardProgressBar } from '@/ui-components/CustomProgress/StandartProgressBar';
 
 export type ProgressStore = {
 	progressStore: {
@@ -20,36 +19,15 @@ type Exercise = DefaultCollectionType & {
 	showAddInfo: boolean;
 };
 
-type TotalProgress = {
-	errorProgress: string[];
-	exam: number;
-	random: {
-		progress: number;
-		resolved: number;
-		unTouch: number;
-	};
-	total: number;
-	takenTestCount: number;
-};
-
-type TotalExercise = {
-	total: number;
-	exam: number;
-	randomProgress: number;
-	randomResolved: number;
-	randomUntouch: number;
-};
-
-export const Progress = () => {
+export const Progress = ({
+	collection,
+	exerciseTheme,
+}: {
+	collection: CollectionsType;
+	exerciseTheme: string;
+}) => {
 	const [progressData, setProgressData] = useState<ProgressStore[]>([]);
 	const [exercise, setExercise] = useState<Exercise[]>([]);
-	const [total, setTotal] = useState<TotalExercise>({
-		total: 0,
-		exam: 0,
-		randomProgress: 0,
-		randomResolved: 0,
-		randomUntouch: 0,
-	});
 
 	useEffect(() => {
 		(async () => {
@@ -57,7 +35,7 @@ export const Progress = () => {
 				const fetchedProgressData = await fetchProgressData();
 				setProgressData(fetchedProgressData);
 
-				const vocabularies: Exercise[] = getAllCollections('vocabularies').map((item) => ({
+				const vocabularies: Exercise[] = getAllCollections(collection).map((item) => ({
 					...item,
 					showAddInfo: false,
 				}));
@@ -67,19 +45,17 @@ export const Progress = () => {
 				console.log('Error fetching progress data: ', err);
 			}
 		})();
-		const totalProgress = calculateTotalProgress();
-		setTotal(totalProgress);
 	}, [progressData.length]);
 
 	const filterData = (exercise: Exercise[]) => {
 		const filteredData = progressData.filter((item) => {
 			const progressStore = item.progressStore;
-			return Object.keys(progressStore).some((key) => key.includes('vocabulary'));
+			return Object.keys(progressStore).some((key) => key.includes(exerciseTheme));
 		});
 
 		const filteredStore = exercise
 			.filter((item) => {
-				const id = `${item.id}_vocabulary`;
+				const id = `${item.id}_${exerciseTheme}`;
 				return filteredData.some((p) => Object.keys(p.progressStore).includes(id));
 			})
 			.map((item) => ({
@@ -99,82 +75,17 @@ export const Progress = () => {
 		setExercise(updateExercise);
 	};
 
-	const calculateTotalProgress = () => {
-		let total = 0;
-		let exam = 0;
-		let randomProgress = 0;
-		let randomResolved = 0;
-		let count = 0;
-		const calculatedProgress: TotalProgress[] = [];
-
-		const data = progressData.filter((p) =>
-			`${Object.keys(p.progressStore)}`.includes('vocabulary'),
-		);
-
-		data.forEach((item) => {
-			const key = Object.keys(item.progressStore)[0];
-			const store = item.progressStore[key];
-
-			if (typeof store === 'object' && store !== null && 'takenTestCount' in store) {
-				const result = calculateProgress(data, key);
-				calculatedProgress.push({
-					...result,
-					takenTestCount: store.takenTestCount.count as number,
-				});
-			}
-		});
-
-		calculatedProgress.forEach((item) => {
-			total += item.total;
-			exam += item.exam;
-			randomProgress += item.random.progress;
-			randomResolved += item.random.resolved;
-			count++;
-		});
-
-		return {
-			total: Math.ceil(total / count) || 0,
-			exam: Math.ceil(exam / count) || 0,
-			randomProgress: Math.ceil(randomProgress / count) || 0,
-			randomResolved: Math.ceil(randomResolved / count) || 0,
-			randomUntouch: Math.ceil(100 - randomProgress - randomResolved) || 0,
-		};
-	};
-
 	return (
-		<div>
-			<p className={styles.progressTitle}>Vocabularies</p>
-			<div className={styles.progressTotalContainer}>
-				<p className={styles.progressTitle}>Total Progress</p>
-				<div className={styles.totalProgress}>
-					<p className={styles.themeTitle}>Total</p>
-					<StandardProgressBar progress={total.total} size="m" fullwidth />
-				</div>
-				<div className={styles.totalProgress}>
-					<p className={styles.themeTitle}>Exam</p>
-					<StandardProgressBar progress={total.exam} size="xs" fullwidth />
-				</div>
-				<div className={styles.totalProgress}>
-					<p className={styles.themeTitle}>Random in resolved</p>
-					<StandardProgressBar progress={total.randomResolved} size="xs" fullwidth />
-				</div>
-				<div className={styles.totalProgress}>
-					<p className={styles.themeTitle}>Random in progress</p>
-					<StandardProgressBar progress={total.randomProgress} size="xs" fullwidth />
-				</div>
-				<div className={styles.totalProgress}>
-					<p className={styles.themeTitle}>Random in untouch</p>
-					<StandardProgressBar progress={total.randomUntouch} size="xs" fullwidth />
-				</div>
-			</div>
+		<div className={styles.exerciseContainer}>
+			<p className={styles.progressTitle}>{exerciseTheme.toLocaleUpperCase()}</p>
 			<div className={styles.progressThemes}>
 				{exercise.map((item) => {
-					const progressId = `${item.id}_vocabulary`;
+					const progressId = `${item.id}_${exerciseTheme}`;
 					const { takenTestCount } =
 						(progressData.find((p) => Object.keys(p.progressStore).includes(progressId))
 							?.progressStore[progressId] as ProgressStoreState) || {};
 
-					const path = `vocabularies/${item.id}`;
+					const path = `${collection}/${item.id}`;
 
 					return (
 						<div key={item.id} className={styles.progressContainer}>
--- src/pages/ProgressPage.tsx
+++ src/pages/ProgressPage.tsx
@@ -1,10 +1,69 @@
 import { Progress } from '@/modules/progress/Progress';
+import { fetchProgressData } from '@/modules/progress/services/fetchProgressData';
+import { ALL_COLLECTIONS, CollectionsType } from '@/shared/constants';
+import { ProgressStoreState } from '@/store/progress';
+import { useEffect, useState } from 'react';
+
+export type ProgressStore = {
+	progressStore: {
+		[key: string]: ProgressStoreState | number;
+	};
+};
+
+type Exercise = {
+	collections: CollectionsType;
+	exercise: string;
+};
 
 export const ProgressPage = () => {
+	const [progressData, setProgressData] = useState<ProgressStore[]>([]);
+	const [exercise, setExercise] = useState<Exercise[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const fetchedProgressData = await fetchProgressData();
+				setProgressData(fetchedProgressData);
+				getKeysExercise(progressData);
+			} catch (err) {
+				console.log('Error fetching progress data: ', err);
+			}
+		})();
+	}, [progressData.length, exercise.length]);
+
+	const getKeysExercise = (progress: ProgressStore[]) => {
+		const keys: Exercise[] = [];
+
+		progress.forEach(({ progressStore }) => {
+			Object.keys(progressStore).forEach((fullKey) => {
+				if (fullKey.includes('timestamp')) return;
+
+				const theme = fullKey.slice(2); // remove prefix
+				const isAlreadyAdded = keys.some(({ exercise }) => exercise.includes(theme));
+				if (isAlreadyAdded) return;
+
+				const collectionKey = Object.keys(ALL_COLLECTIONS).find(
+					(collection) => collection.includes(theme.slice(0, -1)), // crude plural-to-singular match
+				);
+
+				if (collectionKey) {
+					keys.push({
+						exercise: theme,
+						collections: collectionKey as CollectionsType,
+					});
+				}
+			});
+		});
+
+		setExercise(keys);
+	};
+
 	return (
 		<div>
 			<p>Progress</p>
-			<Progress />
+			{exercise.map((item) => (
+				<Progress key={item.exercise} exerciseTheme={item.exercise} collection={item.collections} />
+			))}
 		</div>
 	);
 };
