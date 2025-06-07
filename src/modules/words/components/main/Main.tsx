1a959944e6a98cf55b34d7fc991554ae27598442
Tolik9924 <salurasoft9924@gmail.com>
2025-06-07 18:58:04 +0300

    feat(words page): resolve comments

--- src/modules/words/components/main/Main.tsx
+++ src/modules/words/components/main/Main.tsx
@@ -1,6 +1,6 @@
 import { useWords } from '@/shared/api-hooks/useWords';
 import { Wrapper } from '@/ui-components/Wrapper';
-import { Input } from 'antd';
+import { Input } from '@/ui-components/Input';
 import { useState } from 'react';
 import { WordCard } from '../word-card/WordCard';
 
@@ -8,25 +8,19 @@ import styles from './main.module.css';
 
 const { Search } = Input;
 
-const data = {
-	id: '3c9918b4-e787-4464-8669-d05077c9ee1e',
-	name: 'won',
-	transcription: '/ˈdən/',
-	translate: 'готовий, приготовлений',
-	type: 'verb',
-	useCase: 'cooked or prepared to the desired level of completion',
-	variants: ['done', 'make', 'action'],
+type WordsType = {
+	id: string;
+	name: string;
+	transcription: string;
+	translate: string;
+	type: string;
+	useCase: string;
+	variants: string[];
 };
-
-type WordsType = typeof data;
 
 export const Main = () => {
 	const [word, setWord] = useState<string>('');
 	const { data: words, refetch } = useWords(word);
-	console.log(words, data);
-	console.log(words?.length > 0);
-	console.log('WORDS: ', words);
-	console.log('DATA: ', data);
 	return (
 		<>
 			<Wrapper>
--- src/modules/words/components/word-card/WordCard.tsx
+++ src/modules/words/components/word-card/WordCard.tsx
@@ -1,10 +1,9 @@
 import { Typography } from '@/ui-components/Typography';
+import { classes } from '@/ui-design-atoms/classes';
+import img from '@/assets/download.jpeg';
 
 // eslint-disable-next-line css-modules/no-unused-class
 import styles from './wordCard.module.css';
-
-import img from '@/assets/download.jpeg';
-import { classes } from '@/ui-design-atoms/classes';
 
 export const WordCard = ({
 	name,
@@ -16,28 +15,24 @@ export const WordCard = ({
 	translate: string;
 	type: string;
 	useCase: string;
-}) => {
-	console.log('NAME: ', name);
-
-	return (
-		<div className={styles.card}>
-			<img className={styles.img} src={img} />
-			<div className={styles.content}>
-				<div className={styles.explanation}>
-					<Typography.Title level={4}>{name}</Typography.Title>
-					<div className={styles.translateContainer}>
-						<Typography.Text className={styles.translate}>{translate}</Typography.Text>
-					</div>
-					<div className={styles.useCaseContainer}>
-						<Typography.Text className={styles.useCase}>{useCase}</Typography.Text>
-					</div>
+}) => (
+	<div className={styles.card}>
+		<img className={styles.img} src={img} />
+		<div className={styles.content}>
+			<div className={styles.explanation}>
+				<Typography.Title level={4}>{name}</Typography.Title>
+				<div className={styles.translateContainer}>
+					<Typography.Text className={styles.translate}>{translate}</Typography.Text>
 				</div>
-				<div className={styles.typeContainer}>
-					<span className={classes(styles.type, styles[`${type.toLowerCase()}`])}>
-						{'['} {type} {']'}
-					</span>
+				<div className={styles.useCaseContainer}>
+					<Typography.Text className={styles.useCase}>{useCase}</Typography.Text>
 				</div>
 			</div>
+			<div className={styles.typeContainer}>
+				<span className={classes(styles.type, styles[`${type.toLowerCase()}`])}>
+					{'['} {type} {']'}
+				</span>
+			</div>
 		</div>
-	);
-};
+	</div>
+);
