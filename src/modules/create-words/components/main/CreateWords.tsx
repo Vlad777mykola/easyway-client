961c267ab1f0bbe7bdb4319102bea6c88b3a456b
Tolik9924 <salurasoft9924@gmail.com>
2025-06-20 20:04:18 +0300

    feat(form creating words): create form

--- src/modules/create-words/components/main/CreateWords.tsx
+++ src/modules/create-words/components/main/CreateWords.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Typography } from '@/ui-components/Typography';
 import { FieldGroup } from '@/ui-components/FieldGroup';
 import { Input } from '@/ui-components/Input';
 import { Select } from '@/ui-components/Select';
-import { WrapperCard } from '@/features/wrap-card';
 import { filtersApi } from '@/shared/api/generated';
 import { useWordsMutation } from '../../hooks/useWordsMutation';
 import { schema } from '../../zod-schemas/form.schema';
@@ -26,17 +24,13 @@ export const CreateWords = () => {
 		reset,
 		control,
 		handleSubmit,
-		watch,
 		formState: { errors },
 	} = useForm<FormValues>({
 		resolver: zodResolver(schema),
 	});
 
-	const type = watch('type');
-
 	const onSubmit = (data: FormValues) => {
-		console.log('DATA: ', data);
-		/* mutate(data); */
+		mutate(data);
 	};
 
 	const clearForm = () => {
@@ -44,109 +38,110 @@ export const CreateWords = () => {
 	};
 
 	console.log('FILTERS: ', filters);
-	console.log('TYPE: ', type);
 
 	return (
 		<div className={styles.container}>
 			<Typography.Title className={styles.title} level={2}>
 				Create new word
 			</Typography.Title>
-			<FieldGroup marginY="03" title="Name" error={errors?.name?.message}>
-				<Controller
-					name="name"
-					control={control}
-					render={({ field }) => (
-						<Input
-							{...field}
-							placeholder="Enter your title"
-							size="middle"
-							status={errors?.name && 'error'}
-						/>
-					)}
-				/>
-			</FieldGroup>
-			<FieldGroup marginY="03" title="Transcription" error={errors?.transcription?.message}>
-				<Controller
-					name="transcription"
-					control={control}
-					render={({ field }) => (
-						<Input
-							{...field}
-							placeholder="Enter your description"
-							size="middle"
-							status={errors?.name && 'error'}
-						/>
-					)}
-				/>
-			</FieldGroup>
-			<FieldGroup marginY="03" title="Translate" error={errors?.translate?.message}>
-				<Controller
-					name="translate"
-					control={control}
-					render={({ field }) => (
-						<Input
-							{...field}
-							placeholder="Enter your translate"
-							size="middle"
-							status={errors?.name && 'error'}
-						/>
-					)}
-				/>
-			</FieldGroup>
-			<FieldGroup marginY="03" title="Use Case" error={errors?.useCase?.message}>
-				<Controller
-					name="useCase"
-					control={control}
-					render={({ field }) => (
-						<Input
-							{...field}
-							placeholder="Enter your use case"
-							size="middle"
-							status={errors?.useCase && 'error'}
-						/>
-					)}
-				/>
-			</FieldGroup>
-			<FieldGroup marginY="03" title="Type" error={errors.type?.message}>
-				<Controller
-					name="type"
-					control={control}
-					render={({ field }) => (
-						<Select
-							{...field}
-							placeholder="Select topics"
-							options={types.map((i: string) => ({ label: i, value: i }))}
-							status={errors.type ? 'error' : undefined}
-							onChange={field.onChange}
-							value={field.value}
-						/>
-					)}
-				/>
-			</FieldGroup>
-			<FieldGroup marginY="03" title="Variants" error={errors.type?.message}>
-				<Controller
-					name="variants"
-					control={control}
-					render={({ field }) => (
-						<Select
-							{...field}
-							mode="tags"
-							open={false}
-							suffixIcon={null}
-							placeholder="Press Enter to add tags"
-							status={errors.type ? 'error' : undefined}
-							onChange={field.onChange}
-							value={field.value}
-						/>
-					)}
-				/>
-			</FieldGroup>
+			<div className={styles.formContent}>
+				<FieldGroup marginY="03" title="Name" error={errors?.name?.message}>
+					<Controller
+						name="name"
+						control={control}
+						render={({ field }) => (
+							<Input
+								{...field}
+								placeholder="Enter your title"
+								size="middle"
+								status={errors?.name && 'error'}
+							/>
+						)}
+					/>
+				</FieldGroup>
+				<FieldGroup marginY="03" title="Transcription" error={errors?.transcription?.message}>
+					<Controller
+						name="transcription"
+						control={control}
+						render={({ field }) => (
+							<Input
+								{...field}
+								placeholder="Enter your description"
+								size="middle"
+								status={errors?.name && 'error'}
+							/>
+						)}
+					/>
+				</FieldGroup>
+				<FieldGroup marginY="03" title="Translate" error={errors?.translate?.message}>
+					<Controller
+						name="translate"
+						control={control}
+						render={({ field }) => (
+							<Input
+								{...field}
+								placeholder="Enter your translate"
+								size="middle"
+								status={errors?.name && 'error'}
+							/>
+						)}
+					/>
+				</FieldGroup>
+				<FieldGroup marginY="03" title="Use Case" error={errors?.useCase?.message}>
+					<Controller
+						name="useCase"
+						control={control}
+						render={({ field }) => (
+							<Input
+								{...field}
+								placeholder="Enter your use case"
+								size="middle"
+								status={errors?.useCase && 'error'}
+							/>
+						)}
+					/>
+				</FieldGroup>
+				<FieldGroup marginY="03" title="Type" error={errors.type?.message}>
+					<Controller
+						name="type"
+						control={control}
+						render={({ field }) => (
+							<Select
+								{...field}
+								placeholder="Select topics"
+								options={types.map((i: string) => ({ label: i, value: i }))}
+								status={errors.type ? 'error' : undefined}
+								onChange={field.onChange}
+								value={field.value}
+							/>
+						)}
+					/>
+				</FieldGroup>
+				<FieldGroup marginY="03" title="Variants" error={errors.type?.message}>
+					<Controller
+						name="variants"
+						control={control}
+						render={({ field }) => (
+							<Select
+								{...field}
+								mode="tags"
+								open={false}
+								suffixIcon={null}
+								placeholder="Press Enter to add tags"
+								status={errors.type ? 'error' : undefined}
+								onChange={field.onChange}
+								value={field.value}
+							/>
+						)}
+					/>
+				</FieldGroup>
+			</div>
 			{error && <Typography.Text type="danger">{error?.message}</Typography.Text>}
 			<div className={styles.collectionButtons}>
-				<Button type="primary" onClick={clearForm}>
+				<Button type="primary" shape="round" onClick={clearForm}>
 					Clear
 				</Button>
-				<Button disabled={isPending} type="primary" onClick={handleSubmit(onSubmit)}>
+				<Button disabled={isPending} type="primary" shape="round" onClick={handleSubmit(onSubmit)}>
 					Submit
 				</Button>
 			</div>
