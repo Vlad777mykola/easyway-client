e525422e1d85bb477b7922e262b60116b94de0d0
Vlad777mykola <vlad777mykola@gmail.com>
2026-01-02 21:49:40 +0200

    Feat(gh pages) (#183)
    * fix(dictionary): add words a1-a2
    
    * feat(gh-pages): pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
 export default defineConfig({
 	plugins: [react()],
 	base: '/easyway-client/',
 	resolve: {
 		alias: {
+			// eslint-disable-next-line no-undef
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
